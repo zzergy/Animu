be765825109aa490f53e445f22559f64a813a037
Zergy <xxdarkbluedevilxx@gmail.com>
2019-08-06 21:25:13 +0300

    Minor changes, added todo's

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+//Sticky navigation bar functionality.
 window.onscroll = function () {stickyNav()};
-let navigationBar = document.getElementById('nav-bar');
+const navigationBar = document.getElementById('nav-bar');
 let sticky = navigationBar.offsetTop;
 function stickyNav() {
     if (window.pageYOffset >= sticky) {
@@ -8,3 +9,19 @@ function stickyNav() {
         navigationBar.classList.remove('sticky');
     }
 }
+
+
+//TODO Make it so that when a page is clicked the name of the page on the nav bar is purple.
+/*
+const homeButtonElement = document.getElementById('home-button');
+const animeWikiButtonElement = document.getElementById('anime-wiki-button');
+const wallpapersButtonElement = document.getElementById('wallpapers-button');
+const siteInfoButtonElement = document.getElementById('site-info-button');
+
+navigationBar.addEventListener('click', function () {
+    if (homeButtonElement.style.color === '#8f86f3'){
+
+    }
+});
+
+ */
