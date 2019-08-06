35fdd09874b83c40f402b9ba45262b31a0781ea7
Zergy <xxdarkbluedevilxx@gmail.com>
2019-08-06 12:39:46 +0300

    Added Sticky Navigation bar function

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-/**
- * Created by ZERG on 05/08/2019.
- */
+window.onscroll = function () {stickyNav()};
+let navigationBar = document.getElementById('nav-bar');
+let sticky = navigationBar.offsetTop;
+function stickyNav() {
+    if (window.pageYOffset >= sticky) {
+        navigationBar.classList.add('sticky');
+    } else {
+        navigationBar.classList.remove('sticky');
+    }
+}
